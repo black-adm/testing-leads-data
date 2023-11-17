428691935da573137ca91b2c3d9a3e028d366641
black-adm <mattheuscontato17@gmail.com>
2023-11-17 19:28:28 -0300

    fix: corrections and update organizate folders

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,20 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 
 import { leadsRoutes } from './http/routes/leads'
+import { usersRoutes } from './http/routes/users'
 
 export const app = fastify()
-
-app.register(leadsRoutes, {
-  prefix: 'leads',
-})
 
 app.register(cors, {
   origin: '*',
   methods: ['GET', 'PUT', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type'],
+})
+
+app.register(leadsRoutes, {
+  prefix: 'leads',
+})
+
+app.register(usersRoutes, {
+  prefix: 'user',
 })
--- backend/src/http/routes/leads.ts
+++ backend/src/http/routes/leads.ts
@@ -1,7 +1,6 @@
 import { Lead } from '@prisma/client'
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../../database/client'
-import { User } from '../../entities/user'
 
 export async function leadsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
@@ -26,37 +25,6 @@ export async function leadsRoutes(app: FastifyInstance) {
 
   app.get('/', async () => {
     const list = await prisma.lead.findMany()
-
     return list
-  })
-
-  app.post('/user', async (request, reply) => {
-    try {
-      const user: User = request.body as User
-
-      if (!user.email || !user.role) {
-        reply.code(400).send({
-          success: false,
-          message: 'Dados de usuário incompletos',
-        })
-        return
-      }
-
-      const create = await prisma.user.create({
-        data: user,
-      })
-
-      reply.code(201).send({
-        sucess: true,
-        message: 'Usuário criado com sucesso!',
-        data: create,
-      })
-    } catch (error) {
-      console.error('Erro ao criar usuário:', error)
-      reply.code(500).send({
-        success: false,
-        message: 'Erro ao criar usuário',
-      })
-    }
   })
 }
